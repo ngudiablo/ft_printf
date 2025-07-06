03a435c7aa5c279fcf19fc67cf4f6410f5d53244
ngudiablo <nicolasgualtieriatome@gmail.com>
2025-07-06 12:47:52 +0200

    Add files via upload

---
+++ ft_puthexa_lower.c
@@ -0,0 +1,12 @@
+#include "ft_printf.h"
+
+size_t	ft_puthexa_lower(const unsigned int n)
+{
+	if (n / 16)
+		return (ft_puthexa_lower(n / 16) + ft_puthexa_lower(n % 16));
+	else if (!(n / 10))
+		ft_putchar(n + '0');
+	else
+		ft_putchar((char) n - 10 + 'a');
+	return (1);
+}
